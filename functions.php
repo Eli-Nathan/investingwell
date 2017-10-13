7df04fa95c0a202d996fd66edfc5f98a1e2139cf
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-13 12:37:07 +0100

    Quick changes to try fix the script enqueues

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function startwordpress_scripts() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
 	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
+	 wp_enqueue_script("jquery");
 }
 
 add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
--- index.php
+++ index.php
@@ -179,9 +179,7 @@
      <!-- Bootstrap core JavaScript
     ================================================== -->
      <!-- Placed at the end of the document so the pages load faster -->
-     <script src="js/jquery.min.js"></script>
-     <script src="js/bootstrap.min.js"></script>
-     <script src="js/script.js"></script>
+     
 </body>
 
 </html>
