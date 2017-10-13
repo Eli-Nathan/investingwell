07a72c96728013b7fd101a8af5c273bbf6179a30
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-13 15:56:13 +0100

    Move navbar into header.php

--- header.php
+++ header.php
@@ -15,3 +15,28 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+<body>
+
+     <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container-fluid">
+               <!-- Brand and toggle get grouped for better mobile display -->
+               <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+                         <span class="sr-only">Toggle navigation</span>
+                         <i class="fa fa-bars"></i>
+                    </button>
+               </div>
+               <!-- Collect the nav links, forms, and other content for toggling -->
+               <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                         <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                         <li><a href="#">Link</a></li>
+                         <li><a href="#">Link</a></li>
+                         <li><a href="#">Link</a></li>
+                         <li><a href="#">Link</a></li>
+                    </ul>
+               </div>
+               <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container-fluid -->
+     </nav>
