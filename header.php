3c5c5dea6efb4f476ea83f07348ea4b5d4b715fd
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-13 14:59:35 +0100

    Added header and footer links as well as header and footer definitions

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <title>Investing Well</title>
+     <meta charset="utf-8">
+     <meta name="author" content="Elijah Nathan - Eli's Web">
+     <meta name="description" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href='https://fonts.googleapis.com/css?family=Lato:400,900,300,700' rel='stylesheet' type='text/css'>
+     <?php wp_head(); ?>
+     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+     <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-     <title>Investing Well</title>
-     <meta charset="utf-8">
-     <meta name="author" content="Elijah Nathan - Eli's Web">
-     <meta name="description" content="">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link href='https://fonts.googleapis.com/css?family=Lato:400,900,300,700' rel='stylesheet' type='text/css'>
-     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+<?php get_header(); ?>
 
 <body>
 
@@ -167,19 +152,4 @@
 
      </div>
      <!-- /.container -->
-
-     <footer class="blog-footer">
-          <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-          <p>
-               <a href="#">Back to top</a>
-          </p>
-     </footer>
-
-
-     <!-- Bootstrap core JavaScript
-    ================================================== -->
-     <!-- Placed at the end of the document so the pages load faster -->
-     
-</body>
-
-</html>
+<?php get_header(); ?>
