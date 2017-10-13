e0758c9d7d9f54060aee1bf29fed768e9f9e1add
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-13 15:55:11 +0100

    Remove blog.css and comment out justified nav

--- footer.php
+++ footer.php
@@ -1,14 +1,9 @@
 <footer class="blog-footer">
-     <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-     <p>
-          <a href="#">Back to top</a>
-     </p>
+     <p>Footer text</p>
 </footer>
 
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
+
 <?php wp_footer(); ?>
 </body>
 
