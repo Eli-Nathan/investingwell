65e4f930f222fa8c9119152bb59067088e24b381
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-13 12:48:31 +0100

    And Again

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 // Add scripts and stylesheets
 function include_investingwell_scripts() {
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', false, NULL, 'all' );
 	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
 	 wp_enqueue_script("jquery");
