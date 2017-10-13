fbf020304af3f09b3126bd4e2ca23fa3a9863c70
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-13 12:45:04 +0100

    STILL Trying to fix enqueues

--- functions.php
+++ functions.php
@@ -1,12 +1,11 @@
 <?php
 // Add scripts and stylesheets
-function startwordpress_scripts() {
+function include_investingwell_scripts() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
 	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
 	 wp_enqueue_script("jquery");
 }
-echo "TEST";
-add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
+add_action( 'wp_enqueue_scripts', 'include_investingwell_scripts' );
 
 ?>
