baf6c6bba2a8892983dc204a7445527b39b63562
Elijah Nathan <ely.nathan93@gmail.com>
2017-10-20 20:55:53 +0100

    Altered Styles - added card hover effect and link to card

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <?php
      get_header();
      global $investingwell;
+     global $charity;
+     global $charityname;
+     if(isset($_GET["charity"])) {
+          $charity = $_GET["charity"];
+          if($charity == "chas") {
+               $charityname = "CHAS";
+          }
+          else if($charity == 'oxfam') {
+               $charityname = "Oxfam";
+          }
+          else {
+               $charityname = null;
+          }
+     }
+     else {
+          $charity = null;
+     }
 ?>
 
 
      <!-- Header Ends (Includes navigation bar) -->
      <div class='header col-sm-12'>
           <h1 class='text-center'><?php echo $investingwell; ?></h1>
-          <p>We all have goals and dreams, but how do we get there? Investing Well can help you save towards future goals from childrens education to retirment. This is acheived in a low cost way and can help a charity close to your heart as well. </p>
+          <p>
+               <?php
+                    if($charity == "chas") {
+                         echo "You came from ". $charityname ."! ";
+                    }
+               ?>
+               We all have goals and dreams, but how do we get there? Investing Well can help you save towards future goals from childrens education to retirment. This is acheived in a low cost way and can help a charity close to your heart as well.</p>
           <div class='btnDiv text-center'>
                <a class='btn btn-default btn-iw-p'>
                     Let's Start &nbsp; <i class='fa fa-angle-right'></i>
@@ -27,46 +50,48 @@
          </form>
          <div id='answer'></div>
      </div>
-     <div class='savingFor col-sm-12'>
-          <div class='col-md-offset-2 col-md-8'>
+     <div class='savingFor col-sm-12 col-xs-12'>
+          <div class='col-md-offset-2 col-md-8 col-xs-12'>
                <h3 class='text-center'>Tell us what you are saving for and we can help you reach your goal.</h3>
           </div>
-          <div class='card-options text-center col-sm-12'>
-               <div class='box col-md-2 col-md-offset-1 col-sm-4'>
+          <div class='card-options text-center col-sm-12 col-xs-12'>
+               <a href='#' class='box col-md-2 col-md-offset-1 col-sm-4 col-xs-6'>
                     <div class='box-inner'>
                          <i class='fa fa-bar-chart'></i>
                          <p>Just Investing</p>
                     </div>
-               </div>
-               <div class='box col-md-2 col-sm-4'>
+               </a>
+               <a href='#' class='box col-md-2 col-sm-4 col-xs-6'>
                     <div class='box-inner'>
                          <i class='fa fa-sun-o'></i>
                          <p>Travel</p>
                     </div>
-               </div>
-               <div class='box col-md-2 col-sm-4'>
+               </a>
+               <a href='#' class='box col-md-2 col-sm-4 col-xs-6'>
                     <div class='box-inner'>
                          <i class='fa fa-home'></i>
                          <p>Home</p>
                     </div>
-               </div>
-               <div class='box col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-2'>
+               </a>
+               <a href='#' class='box col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-2 col-xs-6'>
                     <div class='box-inner'>
                          <i class='fa fa-umbrella'></i>
                          <p>Rainy Day</p>
                     </div>
-               </div>
-               <div class='box col-md-2 col-sm-4'>
+               </a>
+               <a href='#' class='box col-md-2 col-sm-offset-0 col-sm-4 col-xs-6 col-xs-offset-3'>
                     <div class='box-inner'>
                          <i class='fa fa-gift'></i>
                          <p>Wedding</p>
                     </div>
+               </a>
+               <div class='col-sm-12 col-xs-12'>
+                    <a class='btn btn-iw-p' href='#'>Let's Start</a>
                </div>
-               <a class='btn btn-iw-p' href='#'>Let's Start</a>
           </div>
      </div>
-     <div class='donations col-sm-12'>
-          <div class='text col-sm-6'>
+     <div class='donations col-sm-12 col-xs-12'>
+          <div class='text col-sm-6 col-xs-12'>
                <h2>Change your world</h2>
                <p>Investing Well offers you the opportunity to make a positive impact on society. We help you plan for your own future and also secure the future of a charity close to your heart. So what are you saving for?</p>
                <a class='btn btn-iw-p' href='#'>Find out more</a>
