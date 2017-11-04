f04d8c9bfbc3060ff72115b28c17a3707d40fac8
Elijah Nathan <ely.nathan93@gmail.com>
2017-11-04 16:54:27 +0000

    Add custom post type

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+// This theme uses post thumbnails
+add_theme_support( 'post-thumbnails' );
 // Add scripts and stylesheets
 function include_investingwell_scripts() {
 	wp_enqueue_style( 'font-awesome-css', get_template_directory_uri() . '/font-awesome/css/font-awesome.min.css' );
@@ -56,5 +58,66 @@ function df_disable_comments_admin_bar() {
     }
 }
 add_action('init', 'df_disable_comments_admin_bar');
+
+
+// Add Charity post type
+function charity_post_type() {
+
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name'                => _x( 'Charities', 'Post Type General Name', 'investingwell' ),
+        'singular_name'       => _x( 'Charity', 'Post Type Singular Name', 'investingwell' ),
+        'menu_name'           => __( 'Charities', 'investingwell' ),
+        'parent_item_colon'   => __( 'Parent Charity', 'investingwell' ),
+        'all_items'           => __( 'All Charities', 'investingwell' ),
+        'view_item'           => __( 'View Cahrity', 'investingwell' ),
+        'add_new_item'        => __( 'Add New Charity', 'investingwell' ),
+        'add_new'             => __( 'Add New', 'investingwell' ),
+        'edit_item'           => __( 'Edit Charity', 'investingwell' ),
+        'update_item'         => __( 'Update Charity', 'investingwell' ),
+        'search_items'        => __( 'Search Charity', 'investingwell' ),
+        'not_found'           => __( 'Not Found', 'investingwell' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'investingwell' ),
+    );
+
+// Set other options for Custom Post Type
+
+    $args = array(
+        'label'               => __( 'charities', 'investingwell' ),
+        'description'         => __( 'Charities we work with', 'investingwell' ),
+        'labels'              => $labels,
+        // Features this CPT supports in Post Editor
+        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        // You can associate this CPT with a taxonomy or custom taxonomy.
+        'taxonomies'          => array( 'types' ),
+        /* A hierarchical CPT is like Pages and can have
+        * Parent and child items. A non-hierarchical CPT
+        * is like Posts.
+        */
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'can_export'          => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+    );
+
+    // Registering your Custom Post Type
+    register_post_type( 'charities', $args );
+
+}
+
+/* Hook into the 'init' action so that the function
+* Containing our post type registration is not
+* unnecessarily executed.
+*/
+
+add_action( 'init', 'charity_post_type', 0 );
 
 ?>
